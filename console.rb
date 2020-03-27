90ee2abbc63de842900726105353deb70dbb0340
John Taggart <taggartj1985@gmail.com>
2020-03-27 11:53:16 +0000

    customers done

---
+++ console.rb
@@ -0,0 +1,30 @@
+require_relative('models/customers')
+require_relative('models/films')
+require_relative('models/tickets')
+
+
+require('pry')
+
+Customer.delete_all()
+
+
+customer1 = Customer.new({'name' => 'Freddy', 'funds' => 100})
+customer1.save
+customer2 = Customer.new({'name' => 'Jason', 'funds' => 20})
+customer2.save
+customer3 = Customer.new({'name' => 'Mike', 'funds' => 60})
+customer3.save
+customer4 = Customer.new({'name' => 'Chucky', 'funds' => 40})
+customer4.save
+
+Customer.order_customers_by_name()
+
+
+
+
+
+
+
+
+binding.pry
+nil
---
+++ models/customers.rb
@@ -0,0 +1,46 @@
+require_relative("../db/sql_runner")
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers (name, funds) VALUES ($1, $2) RETURNING id"
+    values = [@name, @funds]
+    customer = SqlRunner.run(sql, values)[0];
+    @id = customer['id'].to_i
+  end
+
+  def delete()
+    sql = "DELETE FROM customers WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def Customer.order_customers_by_name()
+    sql = "SELECT * FROM customers ORDER BY name"
+    customers = SqlRunner.run(sql)
+    return customers.map{|customer| Customer.new(customer)}
+  end
+
+  def Customer.all()
+    sql = "SELECT * FROM customers"
+    customers = SqlRunner.run(sql)
+    return customers.map{|customer| Customer.new(customer)}
+  end
+
+  def Customer.delete_all()
+    sql = "DELETE FROM customers"
+    SqlRunner.run(sql)
+  end
+
+
+
+end
