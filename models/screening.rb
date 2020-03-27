1c8b261f21521f51fb14b0fb9b771b221d41d149
John Taggart <taggartj1985@gmail.com>
2020-03-27 16:25:41 +0000

    notes to work on

---
+++ models/screening.rb
@@ -0,0 +1,4 @@
+#new class will need CRUD will need time and film_id! starting tickets
+# with zero tickets sold and maybe a capcity limit.
+# don't forget to add it into sql as a table will need to drop before films
+# if its using its ID
